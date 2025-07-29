9b6f56986781216544eae856e6eb142bfa4149f2
Moustapha <amroune.moustapha@gmail.com>
2025-07-29 18:27:56 +0200

    ending stylization

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -28,23 +28,23 @@ const Calculator = () => {
   return (
     <div className="calculator">
       <div id="display"> {currentValue}   </div>
-      <div>
+      <div className='keypad'>
         <button id='clear' className='clear' onClick={() => handleClick("AC")}>AC</button>
-        <button id='divide' className='divide' onClick={() => handleClick("/")}  >/</button>
-        <button id='multiply' className='multiply' onClick={() => handleClick("x")}>x</button>
-        <button id='seven'  className='seven' onClick={() => handleClick("7")} >7</button>
-        <button id='eight' className='eight' onClick={() => handleClick("8")}>8</button>
-        <button id='nine'  className='nine' onClick={() => handleClick("9")} >9</button>
-        <button id='subtract'  className='subtract' onClick={() => handleClick("-")} >-</button>
-        <button id='four' className='four' onClick={() => handleClick("4")}  >4</button>
-        <button id='five' className='five' onClick={() => handleClick("5")} >5</button>
-        <button id='six'  className='six' onClick={() => handleClick("6")} >6</button>
-        <button id='add' className='add' onClick={() => handleClick("+")}  >+</button>
-        <button id='one' className='one' onClick={() => handleClick("1")} >1</button>
-        <button id='two' className='two' onClick={() => handleClick("2")} >2</button>
-        <button id='three' className='three' onClick={() => handleClick("3")} >3</button>
-        <button id='zero' className='zero' onClick={() => handleClick("0")} >0</button>
-        <button id='decimal' className='decimal' onClick={() => handleClick(".")} >.</button>
+        <button id='divide' className='divide operator' onClick={() => handleClick("/")}  >/</button>
+        <button id='multiply' className='multiply operator' onClick={() => handleClick("x")}>x</button>
+        <button id='seven'  className='seven digit' onClick={() => handleClick("7")} >7</button>
+        <button id='eight' className='eight digit' onClick={() => handleClick("8")}>8</button>
+        <button id='nine'  className='nine digit' onClick={() => handleClick("9")} >9</button>
+        <button id='subtract'  className='subtract operator' onClick={() => handleClick("-")} >-</button>
+        <button id='four' className='four digit' onClick={() => handleClick("4")}  >4</button>
+        <button id='five' className='five digit' onClick={() => handleClick("5")} >5</button>
+        <button id='six'  className='six digit' onClick={() => handleClick("6")} >6</button>
+        <button id='add' className='add operator' onClick={() => handleClick("+")}  >+</button>
+        <button id='one' className='one digit' onClick={() => handleClick("1")} >1</button>
+        <button id='two' className='two digit' onClick={() => handleClick("2")} >2</button>
+        <button id='three' className='three digit' onClick={() => handleClick("3")} >3</button>
+        <button id='zero' className='zero digit' onClick={() => handleClick("0")} >0</button>
+        <button id='decimal' className='decimal digit' onClick={() => handleClick(".")} >.</button>
         <button id='equals' className='equals' onClick={() => handleClick("=")}>=</button>
       </div>
     </div>
